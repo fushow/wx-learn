870a143ea3454e2890a83876545f6c100c7c2a86
fuli <787517473qq@.com>
2019-03-13 10:12:52 +0800

    fix:修改navbar切换时选项的字颜色为选中颜色

--- miniprogram/container/pages/navbar/navbar.js
+++ miniprogram/container/pages/navbar/navbar.js
@@ -1,31 +1,34 @@
 // miniprogram/container/pages/navbar/navbar.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-      tabs:[{name:'选项一'},{name:'选项二'},{name:'选项三'},{name:'选项四'}],
-      activeIndex: 0,
-      labelPros:{
-          label:'name'
-      },
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        tabs: [{name: '选项一'}, {name: '选项二'}, {name: '选项三'}, {name: '选项四'}],
+        activeIndex: 0,
+        labelPros: {
+            label: 'name'
+        },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
-    tabEvent(e){
-        console.log('返回值',e.detail.tab)
+    },
+    tabEvent(e) {
+        this.setData({
+            activeIndex: e.detail.activeIndex
+        });
+        console.log('返回值', e.detail)
     }
-})
+});
